a666830e03349ab71cb0b68d69e0660d85c203ff
keremkose <65921942+keremkose@users.noreply.github.com>
2024-12-16 08:54:49 +0100

    Base framework of the application

---
+++ app/models/booking_model.py
@@ -0,0 +1,4 @@
+from sqlalchemy import  Column,ForeignKey
+from app.database import Base
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.sqltypes import String,Integer,Boolean
---
+++ app/routers/booking_controller.py
@@ -0,0 +1,3 @@
+from fastapi import APIRouter
+
+router=APIRouter(prefix="/booking",tags=["booking"]) 
