a8d89080a38e942686ddff2cb5d9f1c91f86ee04
Melissa Basgol <s1109536@student.hsleiden.nl>
2024-12-18 16:47:17 +0100

    our base code

--- app/database_services/user_database_service.py
+++ app/database_services/user_database_service.py
@@ -1,19 +1,23 @@
-from app.schemas.user_schema import UserSchema
+from app.schemas.schemas import UserBase
 from app.models.models import UserModel
 from sqlalchemy.orm.session import Session
 from app.database_services.hash import Hash
 
 
 
-def create_user(user_schema:UserSchema,db:Session):
-    db_user=UserModel(
-        name=user_schema.name,
-        surname=user_schema.surname,
-        username=user_schema.username,
-        email=user_schema.email,
-        password= Hash.bcrypt(user_schema.password)
-    )
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    return 1
+def create_user(user_schema:UserBase,db:Session):
+   db_user=UserModel(
+    name= user_schema.name,
+    surname=user_schema.surname,
+    username=user_schema.username,
+    email=user_schema.email,
+    password=Hash.bcrypt(user_schema.password)
+   )
+   db.add(db_user)
+   db.commit()
+   db.refresh(db_user)
+
+   return db_user
+
+def get_all_users(db:Session):
+   return db.query(UserModel).all()
